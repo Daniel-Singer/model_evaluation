c1dca22129426ea2fe4beb2a71bf99fdcf4daffe
Daniel <singer.daniel85@gmail.com>
2025-02-05 10:50:03 +0100

    the directories for the datasets are changed according to google colab

--- dataset/download.py
+++ dataset/download.py
@@ -7,7 +7,7 @@ def download_training_dataset(download_version):
     
     api_key = os.getenv('ROBOFLOW_API_KEY')
     
-    dataset_location = '../Football-Player-Detection-8'
+    dataset_location = 'content/model_evaluation/Football-Player-Detection-8'
     dataset = None
     # check if dataset exists alread
     if not os.path.exists(dataset_location):
@@ -23,10 +23,15 @@ def download_validation_dataset(download_version):
     
     api_key = os.getenv('ROBOFLOW_API_KEY')
     
-    rf = Roboflow(api_key)
-    project = rf.workspace("vigorelli").project("football-test-dataset")
-    version = project.version(1)
-    dataset = version.download(download_version)
-    return dataset.location
+    dataset_location = 'content/model_evaluation/Football-Player-Detection-8'
+    dataset = None
+    # check if dataset exists alread
+    if not os.path.exists(dataset_location):
+        rf = Roboflow(api_key)
+        project = rf.workspace("vigorelli").project("football-test-dataset")
+        version = project.version(1)
+        dataset = version.download(download_version)
+        return dataset.location
     
+    return dataset_location
 
